c7f51bb728544e1a977c961927b69544498990a7
Flavius Bindea <flav@flav.com>
2015-01-09 19:18:34 +0100

    minor change on RTC test

--- software/hardware_tests/tinyrtc/tinyrtc.ino
+++ software/hardware_tests/tinyrtc/tinyrtc.ino
@@ -24,7 +24,7 @@ void setup () {
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    //rtc.adjust(DateTime(2015, 0, 21, 3, 0, 0));
   }
 }
 
